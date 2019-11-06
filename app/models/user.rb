ab164a28d5d75ba39db1bec24063e1d6176b6393
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-07 00:13:47 +0900

    バリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :full_name, presence: true, length: { maximum: 30}
+  validates :user_name, presence: true, length: { maximum: 30}
+  validates :email, presence: true,
+            length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true,
+            allow_blank: true
+  validates :password, length: { minimum: 8, allow_blank: true }
 end
