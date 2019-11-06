aad3ec5e13ed5bc4e1f1dbe42b4c0881bcdc2988
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-07 00:28:52 +0900

    ユーザーのシードデータを準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  full_name: '山田 太郎',
+  user_name: 'yamada',
+  email: 'yamada@example.com',
+  password: 'password!',
+  password_confirmation: 'password!',
+  image: 'default.png',
+  admin: true
+)
+
+50.times do
+  full_name = Faker::Name.unique.name
+  user_name = Faker::Internet.unique.user_name
+  email = Faker::Internet.unique.email
+  password = 'password'
+  password_confirmation = 'password'
+  image = 'default.png'
+
+  User.create!(
+    full_name: full_name,
+    user_name: user_name,
+    email: email,
+    password: password,
+    password_confirmation: password_confirmation,
+    image: image
+  )
+end
