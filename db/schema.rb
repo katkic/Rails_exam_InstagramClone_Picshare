c06d793a79ed2833eb193cb82ef6609e4f7689d6
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-07 00:05:27 +0900

    usersテーブルのemailにユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2019_11_05_123828) do
+ActiveRecord::Schema.define(version: 2019_11_06_150133) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_123828) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posts", "users"
