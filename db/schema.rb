2450d67abeebc8f36bf01c73e69b2b6de8ffd1fb
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-07 22:44:36 +0900

    usersテーブルのimageカラムに、デフォルト値を設定

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,18 @@
-ActiveRecord::Schema.define(version: 2019_11_06_150133) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_11_07_132429) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
@@ -15,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_150133) do
     t.string "user_name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.text "image", null: false
+    t.text "image", default: "default.png", null: false
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
