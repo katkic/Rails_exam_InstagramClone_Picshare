c0a75352671138671f248c1cc68bef79faaeaefc
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-10 23:10:04 +0900

    postsテーブルのimageとcaptionに、not null制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_132429) do
+ActiveRecord::Schema.define(version: 2019_11_10_135551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "image"
-    t.text "caption"
+    t.text "image", null: false
+    t.text "caption", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
