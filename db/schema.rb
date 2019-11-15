3dba2169d30974f5818d34120096f85bd89213ca
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-15 23:07:54 +0900

    favoritesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_164007) do
+ActiveRecord::Schema.define(version: 2019_11_14_150841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "image", null: false
