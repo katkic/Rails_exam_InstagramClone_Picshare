abd38948331b4b644cf38b32e54c9582cc6a5f0a
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-19 00:45:23 +0900

    コード修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
