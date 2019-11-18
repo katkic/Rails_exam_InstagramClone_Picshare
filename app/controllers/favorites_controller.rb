3e08f9db54826a6292b542b2072b1c3bfd14f922
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-18 22:52:10 +0900

    お気に入り登録、削除後の遷移先を変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,11 +5,11 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to post_path(favorite.post_id), notice: "#{favorite.post.user.user_name}さんの投稿をお気に入り登録しました"
+    redirect_to favorites_path, notice: "#{favorite.post.user.user_name}さんの投稿をお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to post_path(favorite.post_id), notice: "#{favorite.post.user.user_name}さんの投稿をお気に入り削除しました"
+    redirect_to favorites_path, notice: "#{favorite.post.user.user_name}さんの投稿をお気に入り削除しました"
   end
 end
